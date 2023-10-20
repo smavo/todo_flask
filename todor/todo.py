5bb4cd70623a12bcf0eb4e4be009f3d3c3431516
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-20 18:16:28 -0500

    Add Update Tarea

--- todor/todo.py
+++ todor/todo.py
@@ -29,3 +29,24 @@ def create():
         return redirect(url_for('todo.list'))
     return render_template('todo/create.html')
 
+
+def get_todo(id):
+    todo = Todo.query.get_or_404(id)
+    return todo
+
+
+@bp.route('/update/<int:id>', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    todo = get_todo(id)
+
+    if request.method == 'POST':
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
+        todo.state = True if request.form.get('state') == 'on' else False
+
+        db.session.commit()
+        return redirect(url_for('todo.list'))
+    return render_template('todo/update.html', todo=todo)
+
+
