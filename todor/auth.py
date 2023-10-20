8140fb6b4cad24e3ddb7fdadf074a7dd93e95bba
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-20 15:46:04 -0500

    Add logout

--- todor/auth.py
+++ todor/auth.py
@@ -66,3 +66,9 @@ def load_logged_in_user():
         g.user = None
     else:
         g.user = User.query.get_or_404(user_id)
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
