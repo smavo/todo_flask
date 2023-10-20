03d8cc7471683ecb4ff59dd21461bff1fc9f7fa8
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-20 15:44:45 -0500

    Add validacion navbar, verificar session

--- todor/todo.py
+++ todor/todo.py
@@ -3,6 +3,8 @@ from flask import Blueprint, render_template
 from .models import Todo, User
 from todor import db
 
+from .models import Todo
+from todor import db
 
 bp = Blueprint('todo', __name__, url_prefix='/todo')
 
@@ -10,6 +12,6 @@ bp = Blueprint('todo', __name__, url_prefix='/todo')
 @bp.route('/list')
 def index():
     todos = Todo.query.all()
-    return render_template('todo/index.html', todos = todos)
+    return render_template('todo/index.html', todos=todos)
 
 
