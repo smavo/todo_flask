7fb5511ddeae5516f168fa541b0777c174d0ee2f
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-10-19 23:35:21 -0500

    Registro de Usuario

--- todor/auth.py
+++ todor/auth.py
@@ -1,12 +1,34 @@
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, url_for, redirect, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+from .models import User
+from todor import db
 
 
-@bp.route('/register')
+@bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User(username, generate_password_hash(password))
+
+        error = None
+
+        user_name = User.query.filter_by(username=username).first()
+
+        if user_name == None:
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('auth.login'))
+        else:
+            error = f'El usuario {username} ya esta registrado'
+
+        flash(error)
+
     return render_template('auth/register.html')
 
 
